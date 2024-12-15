10f319078fc51311c649f2aac6608de12e45fadb
Burak Ince <burak.ince@linux.org.tr>
2024-12-15 11:40:32 +0100

    Add 3 seconds wait time for flask

--- tests/test_basic_auth_postgres_integration.py
+++ tests/test_basic_auth_postgres_integration.py
@@ -1,5 +1,6 @@
 import os
 import re
+import time
 
 import mlflow
 from mlflow.server.auth.client import AuthServiceClient
@@ -34,6 +35,7 @@ def test_postgres_backended_model_upload_and_access_with_basic_auth(
 
         log_message = f".*Listening at: {re.escape(base_url)}.*"
         compose.wait_for_logs("mlflow", log_message)
+        time.sleep(3)  # Wait 3 seconds more the get flask ready
 
         experiment_name = "aws-cloud-postgres-experiment"
         model_name = "test-aws-pg-model"
@@ -41,8 +43,8 @@ def test_postgres_backended_model_upload_and_access_with_basic_auth(
         os.environ["MLFLOW_TRACKING_USERNAME"] = mlflow_admin_username
         os.environ["MLFLOW_TRACKING_PASSWORD"] = mlflow_admin_password
 
-        client = AuthServiceClient(base_url)
-        client.create_user(mlflow_user_username, mlflow_user_password)
+        mlflow_auth_client = AuthServiceClient(base_url)
+        mlflow_auth_client.create_user(mlflow_user_username, mlflow_user_password)
 
         os.environ["MLFLOW_TRACKING_USERNAME"] = mlflow_user_username
         os.environ["MLFLOW_TRACKING_PASSWORD"] = mlflow_user_password
@@ -61,8 +63,8 @@ def test_postgres_backended_model_upload_and_access_with_basic_auth(
             model_uri = f"runs:/{run.info.run_id}/model"
             model_details = mlflow.register_model(model_uri, model_name)
 
-            client = MlflowClient()
-            client.set_registered_model_alias(
+            mlflow_client = MlflowClient()
+            mlflow_client.set_registered_model_alias(
                 name=model_details.name,
                 alias=stage_name,
                 version=model_details.version,
