0572c88e14906b64cc9351b1def47302495494ee
Burak Ince <burak.ince@linux.org.tr>
2025-09-17 20:11:32 +0200

    Mlflow SQL Server Support (#313)
    * Add pymssql driver to the mlflow image
    
    * Add tests for AWS cloud and GCP cloud SQL Server integrity

---
+++ tests/integration/test_aws_cloud_mssql_integration.py
@@ -0,0 +1,59 @@
+import os
+
+import requests
+from testcontainers.compose import DockerCompose
+
+import mlflow
+from mlflow import MlflowClient
+
+
+def test_mssql_backended_aws_simulation_model_upload_and_access_via_api(
+    test_model, training_params, conda_env
+):
+    with DockerCompose(
+        context=".",
+        compose_file_name=["docker-compose.aws-mssql-test.yaml"],
+        # pull=True,
+        build=True,
+    ) as compose:
+        mlflow_host = compose.get_service_host("mlflow", 8080)
+        mlflow_port = compose.get_service_port("mlflow", 8080)
+        minio_host = compose.get_service_host("minio", 9000)
+        minio_port = compose.get_service_port("minio", 9000)
+
+        base_url = f"http://{mlflow_host}:{mlflow_port}"
+
+        compose.wait_for(base_url)
+
+        experiment_name = "aws-cloud-mssql-experiment"
+        model_name = "test-aws-mssql-model"
+        stage_name = "Staging"
+        mlflow.set_tracking_uri(base_url)
+        mlflow.set_experiment(experiment_name)
+        experiment = mlflow.get_experiment_by_name(experiment_name)
+
+        os.environ["MLFLOW_S3_ENDPOINT_URL"] = f"http://{minio_host}:{minio_port}"
+        os.environ["AWS_ACCESS_KEY_ID"] = "minioadmin"
+        os.environ["AWS_SECRET_ACCESS_KEY"] = "minioadmin"
+
+        with mlflow.start_run(experiment_id=experiment.experiment_id) as run:
+            mlflow.log_params(training_params)
+            mlflow.pyfunc.log_model("model", conda_env=conda_env, python_model=test_model)
+            model_uri = f"runs:/{run.info.run_id}/model"
+            model_details = mlflow.register_model(model_uri, model_name)
+
+            client = MlflowClient()
+            client.set_registered_model_alias(
+                name=model_details.name,
+                alias=stage_name,
+                version=model_details.version,
+            )
+
+        params = {"name": model_name, "alias": stage_name}
+        latest_version_url = f"{base_url}/api/2.0/mlflow/registered-models/alias"
+        r = requests.get(url=latest_version_url, params=params, timeout=300)
+
+        assert model_name == r.json()["model_version"]["name"]
+        assert "1" == r.json()["model_version"]["version"]
+        assert "READY" == r.json()["model_version"]["status"]
+        assert "Staging" == r.json()["model_version"]["aliases"][0]
---
+++ tests/integration/test_gcp_cloud_mssql_integration.py
@@ -0,0 +1,58 @@
+import os
+
+import requests
+from testcontainers.compose import DockerCompose
+
+import mlflow
+from mlflow import MlflowClient
+
+
+def test_mssql_backended_gcp_simulation_model_upload_and_access_via_api(
+    test_model, training_params, conda_env
+):
+    with DockerCompose(
+        context=".",
+        compose_file_name=["docker-compose.gcp-mssql-test.yaml"],
+        # pull=True,
+        build=True,
+    ) as compose:
+        mlflow_host = compose.get_service_host("mlflow", 8080)
+        mlflow_port = compose.get_service_port("mlflow", 8080)
+        gcs_host = compose.get_service_host("gcs", 4443)
+        gcs_port = compose.get_service_port("gcs", 4443)
+
+        base_url = f"http://{mlflow_host}:{mlflow_port}"
+
+        compose.wait_for(base_url)
+
+        experiment_name = "gcp-cloud-mssql-experiment"
+        model_name = "test-gcp-mssql-model"
+        stage_name = "Staging"
+        mlflow.set_tracking_uri(base_url)
+        mlflow.set_experiment(experiment_name)
+        experiment = mlflow.get_experiment_by_name(experiment_name)
+
+        os.environ["STORAGE_EMULATOR_HOST"] = f"http://{gcs_host}:{gcs_port}"
+        os.environ["GOOGLE_CLOUD_PROJECT"] = "mlflow"
+
+        with mlflow.start_run(experiment_id=experiment.experiment_id) as run:
+            mlflow.log_params(training_params)
+            mlflow.pyfunc.log_model("model", conda_env=conda_env, python_model=test_model)
+            model_uri = f"runs:/{run.info.run_id}/model"
+            model_details = mlflow.register_model(model_uri, model_name)
+
+            client = MlflowClient()
+            client.set_registered_model_alias(
+                name=model_details.name,
+                alias=stage_name,
+                version=model_details.version,
+            )
+
+        params = {"name": model_name, "alias": stage_name}
+        latest_version_url = f"{base_url}/api/2.0/mlflow/registered-models/alias"
+        r = requests.get(url=latest_version_url, params=params, timeout=300)
+
+        assert model_name == r.json()["model_version"]["name"]
+        assert "1" == r.json()["model_version"]["version"]
+        assert "READY" == r.json()["model_version"]["status"]
+        assert "Staging" == r.json()["model_version"]["aliases"][0]
